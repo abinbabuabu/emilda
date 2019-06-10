7295aa452ed1eca97915de52077f13118e42d33d
Peter Paul Arasu <peterpaularasu@gmail.com>
2019-06-10 19:27:29 +0530

    OTP screen done

---
+++ app/src/main/java/com/emilda/emilda/OtpScreen.kt
@@ -0,0 +1,12 @@
+package com.emilda.emilda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtpScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp_screen)
+    }
+}
