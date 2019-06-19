21a4a930af6521a0f6d647087c88862e6fb51890
Peter Paul Arasu <peterpaularasu@gmail.com>
2019-06-20 02:42:40 +0530

    Fixing Navigation (Before)

--- app/src/main/java/com/emilda/emilda/MainActivities/DetailsActivity.kt
+++ app/src/main/java/com/emilda/emilda/MainActivities/DetailsActivity.kt
@@ -1,54 +1,30 @@
 package com.emilda.emilda.MainActivities
 
 import android.content.Intent
-import android.graphics.Point
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.transition.Explode
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.fragment.findNavController
 import com.emilda.emilda.R
-import com.emilda.emilda.Adapters.WorksAdapter
-import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_details.*
-import kotlinx.android.synthetic.main.app_bar_details.*
-import kotlinx.android.synthetic.main.content_details.*
-import setUpBottomAppBarShapeAppearance
+import kotlinx.android.synthetic.main.main.*
 import java.net.URLEncoder
 
 
 class DetailsActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        setContentView(R.layout.main)
         exitEnterAnim()
 
-        val point = Point()
-        val size = windowManager.defaultDisplay.getSize(point)
-        card_recycle_view.overScrollMode = View.OVER_SCROLL_NEVER
-        card_recycle_view.layoutManager = GridLayoutManager(this, 2)
-        card_recycle_view.adapter = WorksAdapter(point.y)
+        val controller = main_nav_fragment.findNavController()
 
-        black_bg.layoutParams.height = point.y/3
+        controller.navigate()
 
-        setUpBottomAppBarShapeAppearance(fab_details, bottom_bar)
-        bottom_msg.setOnClickListener {
-            whatsApp()
-        }
-
-        setPortfolioFragment()
 
     }
 
@@ -84,23 +60,29 @@ class DetailsActivity : AppCompatActivity() {
     }
 
 
-    fun setPortfolioFragment() {
-        val navController = Navigation.findNavController(this, R.id.portfolio_nav_frag)
-        all_button.setOnClickListener {
-            navController.popBackStack()
-            navController.navigate(R.id.allPorfolio)
+//    fun setPortfolioFragment() {
+//        val navController = Navigation.findNavController(this, R.id.portfolio_nav_frag)
+//        all_button.setOnClickListener {
+//            navController.popBackStack()
+//            navController.navigate(R.id.allPorfolio)
+//
+//
+//        }
+//        design_button.setOnClickListener {
+//            navController.popBackStack()
+//            navController.navigate(R.id.designFragment)
+//
+//        }
+//        dev_button.setOnClickListener {
+//            navController.popBackStack()
+//            navController.navigate(R.id.devFragment)
+//
+//        }
+//    }
 
 
-        }
-        design_button.setOnClickListener {
-            navController.popBackStack()
-            navController.navigate(R.id.designFragment)
 
-        }
-        dev_button.setOnClickListener {
-            navController.popBackStack()
-            navController.navigate(R.id.devFragment)
 
-        }
-    }
+
+
 }
